b506d6377531415513e68ba0d2409b118031c1dd
antom <antom@MAIBENBEN-PC>
2024-12-11 19:48:57 +0800

    Добавлено много всего:Начиная от главной страницы до страницы с отелями которые можно добавлять,изменять,удалять

--- WpfApp3/AddEditPage.xaml.cs
+++ WpfApp3/AddEditPage.xaml.cs
@@ -20,15 +20,56 @@ namespace WpfApp3
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private int[] massive = {1, 2, 3, 4, 5 };
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage(Hotel selectedHotel)
         {
             InitializeComponent();
+            if (selectedHotel != null) 
+            {
+                _currentHotel = selectedHotel; 
+            }
             ComboBox.ItemsSource = Import_FileEntities.GetContext().Countries.ToList();
+            ComboBoxList.ItemsSource = massive;
+            DataContext = _currentHotel;
         }
-
+    
         private void BtnSave_click(object sender, RoutedEventArgs e)
         {
-
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+            {
+                errors.AppendLine("Укажите название отеля");
+            }
+            if (_currentHotel.CountOfStars == 0)
+            {
+                errors.AppendLine("Выберите количество звёзд");
+            }
+            if (_currentHotel.Country == null)
+            {
+                errors.AppendLine("Выберите страну");
+            }
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentHotel.ID == 0)
+            {
+                Import_FileEntities.GetContext().Hotels.Add(_currentHotel);
+            }
+            try
+            {
+                Import_FileEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация успешно сохранена!");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
+
+     
     }
 }
--- WpfApp3/HotelsPage.xaml.cs
+++ WpfApp3/HotelsPage.xaml.cs
@@ -23,26 +23,51 @@ namespace WpfApp3
         public HotelsPage()
         {
             InitializeComponent();
-            DGridHotels.ItemsSource = Import_FileEntities.GetContext().Hotels.ToList();
+            //DGridHotels.ItemsSource = Import_FileEntities.GetContext().Hotels.ToList();
         }
 
-        private void OnClick(object sender, RoutedEventArgs e)
+        /*private void OnClick(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            Manager.MainFrame.Navigate(new AddEditPage(null));
         }
+        */
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
 
         }
 
         private void DtnAdd_click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            Manager.MainFrame.Navigate(new AddEditPage(null));
         }
 
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
         {
-
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {hotelsForRemoving.Count()} элементов?","Внимание",
+                MessageBoxButton.YesNo,MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                    {
+                    Import_FileEntities.GetContext().Hotels.RemoveRange(hotelsForRemoving);
+                    Import_FileEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно удалены!");
+                    DGridHotels.ItemsSource = Import_FileEntities.GetContext().Hotels.ToList();
+                    }
+                catch (Exception ex)
+                    {
+                    MessageBox.Show(ex.Message.ToString());
+                    }
+            }
+        }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Import_FileEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = Import_FileEntities.GetContext().Hotels.ToList();
+            }
         }
     }
 }
