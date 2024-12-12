58a2d40ca1a64eacfc581678993a803de59ecf99
antom <antom@MAIBENBEN-PC>
2024-12-12 12:47:20 +0800

    Добавлено много всего .Почти всё сделано осталось только страницу входа создать

--- WpfApp3/ToursPage.xaml.cs
+++ WpfApp3/ToursPage.xaml.cs
@@ -33,29 +33,40 @@ namespace WpfApp3
             ComboType.ItemsSource = allTypes;
             CheckActual.IsChecked = true;
             ComboType.SelectedIndex = 0;
-            var currentTours = Import_FileEntities.GetContext().Tours.ToList();
-            LViewTours.ItemsSource = currentTours;
-            Images = new ObservableCollection<BitmapImage>();
-            DataContext = this;
-            
-            
-        }
+            UpdateTours();
 
+            DataContext = this;
+        }
+        private void UpdateTours()
+        {
+            var currentTours = Import_FileEntities.GetContext().Tours.ToList();
+            if (ComboType.SelectedIndex > 0)
+            {
+                currentTours = currentTours.Where(p => p.Types.Contains(ComboType.SelectedItem as Type)).ToList();
+            }
+            currentTours = currentTours.Where(p => p.Name.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            if (CheckActual.IsChecked.Value) 
+            {
+                currentTours = currentTours.Where(p => p.IsActual).ToList();
+            }
+            LViewTours.ItemsSource = currentTours.OrderBy(p => p.TicketCount).ToList();
+        }
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            UpdateTours();
         }
 
 
         private void CheckActual_Checked(object sender, RoutedEventArgs e)
         {
-
+            UpdateTours();
         }
 
         private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            UpdateTours();
         }
+        
     }
 }
 
